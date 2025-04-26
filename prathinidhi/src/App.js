26cc7b4ed0592f6f62fed12e09ed2d51fb37c6ee
Vinay <vinayvelpula03@gmail.com>
2025-04-26 22:00:02 +0530

    scaffolding done

---
+++ prathinidhi/src/App.js
@@ -0,0 +1,15 @@
+// App.js
+
+import React from 'react';
+import Chatbot from './components/Chatbot'; // Importing your Chatbot component
+import './App.css'; // Optional if you want to keep default CSS or your own
+
+function App() {
+  return (
+    <div className="App">
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
